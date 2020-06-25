9d9d18f75eef63ecb461bb5b4d12e19f1a16779b
William <willlin1996@gmail.com>
2020-06-25 19:26:15 -0400

    save

--- app/controllers/api/v1/decks_controller.rb
+++ app/controllers/api/v1/decks_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::DecksController < ApplicationController
+    def index 
+        @decks = Deck.all
+        render json: @decks
+    end
+
+    def show
+        @deck = Deck.find(params[:id])
+        render json: @deck
+    end
+
+    def create 
+        @deck = Deck.create(deck_params)
+        render json: @deck
+    end
+
+    def update 
+        @deck = Deck.find(params[:id])
+        @deck.update(deck_params)
+        render json: @deck
+    end
+
+    private 
+
+    def deck_params 
+        params.require(:deck).permit(:user_id, :name)
+    end
 end
--- app/controllers/api/v1/flashcards_controller.rb
+++ app/controllers/api/v1/flashcards_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::FlashcardsController < ApplicationController
+    def index 
+        @flashcards = Flashcard.all 
+        render json: @flashcards 
+    end 
+
+    def show 
+        @flashcard = Flashcard.find(params[:id])
+        render json: @flashcard
+    end 
+
+    def create 
+        @flashcard = Flashcard.create(flashcard_params)
+        render json: @flashcard
+    end 
+
+    def update 
+        @flashcard = Flashcard.find(params[:id])
+        render json: @flashcard
+    end 
+
+    def destroy
+        @flashcard = Flashcard.find(params[:id])
+        @flashcard.destroy
+    end
+
+    private 
+
+    def flashcard_params
+        params.require(:flashcard).permit(:question, :answer, :deck_id)
+    end
 end
